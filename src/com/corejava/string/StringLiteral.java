4e59879db652277e6f13468627d93a470358e128
Thanoojgithub <javaeecub@gmail.com>
2015-08-22 08:41:37 +0530

    String literals code changes

--- src/com/corejava/string/StringLiteral.java
+++ src/com/corejava/string/StringLiteral.java
@@ -5,20 +5,20 @@ public class StringLiteral {
 	public static void main(String[] args) {
 
 		String str1 = "hello6";
+		String str2 = "hello" + 6;
+		String str3 = "hello" + "" + 6;
+		String str4 = "hello" + str1.length();
+		String str5 = str1;
 		String str6 = "6";
-		String str2 = "hello" + str6;
-		String str3 = "hello" + 6;
-		String str4 = "hello" + "" + 6;
-		String str5 = "hello" + str1.length();
-		String str7 = str1;
+		String str7 = "hello" + str6;
 		String str8 = "hello" + "6";
 
-		System.out.println(str1 == str2);// false
+		System.out.println(str1 == str2);// true
 		System.out.println(str1 == str3);// true
-		System.out.println(str1 == str4);// true
-		System.out.println(str1 == str5);// false
+		System.out.println(str1 == str4);// false
+		System.out.println(str1 == str5);// true
 		System.out.println(str1 == str6);// false
-		System.out.println(str1 == str7);// true
+		System.out.println(str1 == str7);// false
 		System.out.println(str1 == str8);// true
 	}
 
