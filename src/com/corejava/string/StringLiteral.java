8eaeca99c8e0259f2ab5edd5206c514cabd26dcb
javaeecub@gmail.com <javaeecub@gmail.com>
2015-08-25 22:07:47 +0530

    String literal and sample threads

--- src/com/corejava/string/StringLiteral.java
+++ src/com/corejava/string/StringLiteral.java
@@ -1,5 +1,8 @@
 package com.corejava.string;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class StringLiteral {
 
 	public static void main(String[] args) {
@@ -20,6 +23,29 @@ public class StringLiteral {
 		System.out.println(str1 == str6);// false
 		System.out.println(str1 == str7);// true
 		System.out.println(str1 == str8);// true
+		
+		
+		
+
+		String str = "aaaabbbbbbccccccccpqrstv";
+		// OUTPUT : a4b6c8pqrstv
+		System.out.println(charWithCountAsAString(str));
 	}
 
+	private static String charWithCountAsAString(String str) {
+		char[] ch = str.toCharArray();
+		Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+		for (char c : ch) {
+			map.put(c, map.containsKey(c) ? map.get(c) + 1 : 1);
+		}
+		StringBuilder sb = new StringBuilder();
+		for (Map.Entry<Character, Integer> d : map.entrySet()) {
+			if (d.getValue() > 1) {
+				sb.append(d.getKey()).append(d.getValue());
+			} else {
+				sb.append(d.getKey());
+			}
+		}
+		return sb.toString();
+	}
 }
