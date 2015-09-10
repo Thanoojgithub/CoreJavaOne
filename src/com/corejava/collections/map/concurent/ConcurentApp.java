7855928ce878c2477db2e5bc6065bbe7bf57f5da
javaeecub@gmail.com <javaeecub@gmail.com>
2015-09-10 22:00:23 +0530

    ConcurrentMap debug

--- src/com/corejava/collections/map/concurent/ConcurentApp.java
+++ src/com/corejava/collections/map/concurent/ConcurentApp.java
@@ -1,9 +1,7 @@
 package com.corejava.collections.map.concurent;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -27,22 +25,31 @@ public class ConcurentApp {
 			iterator.remove(); 
 			/*
 			 * java.util.ConcurrentModificationException
+			 * maps.remove(iterator.next());
 			 */
-			//maps.remove(iterator.next());
 		}
 		
+		
+		
 		Map<String, String> concMaps = new ConcurrentHashMap<String, String>();
-		concMaps.put("a", "a"); 
-		concMaps.put("1", "1"); 
-		concMaps.put("b", "b");
-		concMaps.put("c", "c");
+		concMaps.put("BBBB", "BBBB");
+		concMaps.put("AaBB", "AaBB");
+		concMaps.put("BBAa", "BBAa");
+		concMaps.put("keyOne", "valueOne");
+		concMaps.put("keyTwo", "valueTwo");
+		concMaps.put("keyThree", "valueThree");
 		for (Map.Entry<String, String> entry : concMaps.entrySet()) {
 			System.out.println(entry.getKey() + " | "+entry.getValue());
 		}
-		for (Iterator<String> iterator2 = keySet.iterator(); iterator2.hasNext();) {
+		
+		System.out.println("----------remove----and----add same------------");
+		for (Iterator<String> iterator2 = concMaps.keySet().iterator(); iterator2.hasNext();) {
 			String string = (String) iterator2.next();
-			concMaps.remove(string); // here, NO Exception
-			concMaps.put(string, string);
+			concMaps.remove(string);
+			System.out.print("an entry, ");
+			System.out.println("removed :: "+string);
+			// here, NO Exception has thrown
+			//concMaps.put(string, string);
 		} 
 		
 		for (Map.Entry<String, String> entry : concMaps.entrySet()) {
